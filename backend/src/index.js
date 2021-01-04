97aa7d298482139d665498457c164e8e12bd962f
lucijr <lucianoqazwsx@gmail.com>
2021-01-04 16:29:16 -0300

    chore: Created routes using HTTP methods. GET: Search for information on the backend. POST: Create information on the backend. PUT / PATCH: Change information on the backend. DELETE: Delete information on the backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,43 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request,response) => {
-    return response.json({message : 'Hello World!!'});
+/**
+ * HTTP methods:
+ * 
+ * GET: Search for information on the backend.
+ * POST: Create information on the backend.
+ * PUT/PATCH: Change information on the backend. 
+ * DELETE: Delete information on the backend. 
+ */
+
+app.get('/projects', (request,response) => {
+    return response.json([
+        'Project 1',
+        'Project 2'
+    ]);
+});
+
+app.post('/projects', (request,response) => {
+    return response.json([
+        'Project 1',
+        'Project 2',
+        'Project 3'
+    ]);
+});
+
+app.put('/projects/:id', (request,response) => {
+    return response.json([
+        'Project 4',
+        'Project 2',
+        'Project 3'
+    ]);
+});
+
+app.put('/projects/:id', (request,response) => {
+    return response.json([
+        'Project 2',
+        'Project 3'
+    ]);
 });
 
 app.listen(3333, () =>{
