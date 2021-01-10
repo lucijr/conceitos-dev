8099eb82c49a154aa614cd70df3cf5563480a7e8
lucijr <lucianoqazwsx@gmail.com>
2021-01-10 07:06:52 -0300

    feat: Base project frontend with React.js. Using command yarn add react react-dom && yarn add @babel/core @babel/preset-env @babel/preset-react @babel-cli webpack webpack-cli. Babel configured. File babel.config.js is default for everyone projects.

---
+++ frontend/babel.config.js
@@ -0,0 +1,8 @@
+const { Module } = require("webpack");
+
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-react'
+    ],
+}
---
+++ frontend/public/script.js
@@ -0,0 +1,10 @@
+/**
+ * Babel: Convert (transpiler) React code to a code that the browse understands.
+ * Webpack: For each type of file (.js, .css, .png) it converts the code in a different way.
+ * 
+ * Loaders: babel-loader, css-loader, image-loader.
+ * 
+ * Commands: yarn add @babel/core @babel/preset-env @babel/preset-react @babel-cli webpack webpack-cli
+ * 
+ * 
+ */
