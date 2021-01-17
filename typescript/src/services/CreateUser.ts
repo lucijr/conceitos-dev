4840db92a33e312db55b5b711a9c9bf63039c59a
lucijr <lucianoqazwsx@gmail.com>
2021-01-17 12:04:49 -0300

    chore: Finish concepts typescript. Created folder services. Created file CreateUser.ts in services. Concepts of interface in typescript.

---
+++ typescript/src/services/CreateUser.ts
@@ -0,0 +1,25 @@
+/**
+ * To create: name, email, password.
+ */
+
+interface techObject{
+    title: string;
+    experience: number;
+}
+
+interface CreateUserData {
+    name?: string;
+    email: string;
+    password: string;
+    techs: Array<string | techObject>
+}
+
+export default function createUser({name, email, password}: CreateUserData) {
+    const user = {
+        name,
+        email,
+        password
+    }
+
+    return user;
+}
